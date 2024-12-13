4f5fd5a03f6bfff3d58e283c654f836455ac04b3
RandieW06 <stu.randie.williams@ihsnola.org>
2024-12-13 21:12:18 +0000

    completed bouncing box project

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,9 +38,9 @@ $(function () {
     // Create platforms
    // You must decide the x position, y position, width, and height of the platforms
    // example usage: createPlatform(x,y,width,height)
-   createPlatform(450, 400);
-   createPlatform(400, 570);
-   createPlatform(800, 320);
+   createPlatform(450, 470, 150, 20);
+   createPlatform(400, 600, 400, 150);
+   createPlatform(485, 400);
    createPlatform(50, 490);
    createPlatform(750, 600);
 
@@ -52,9 +52,9 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-    creatCollectable(400, 570);
-    createCollectable(450, 500);
-    createCollectable(490, 550);
+    createCollectable("database", 400, 570);
+    createCollectable("database", 450, 500);
+    createCollectable("database", 490, 550);
 
 
 
@@ -64,9 +64,10 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-    createCannon(500, 0);
-    createCannon(0, 500);
-    createCannon(490, 0);
+    createCannon( 30, 0);
+    createCannon( 0, 10);
+    createCannon( 20, 0);
+    
 
 
 
